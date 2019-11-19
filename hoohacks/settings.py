022307af2847a4d54071f471654dba1874b9aa05
Rithik Yelisetty <rithik@gmail.com>
2019-11-19 14:44:40 -0500

    Try SSL forcing

--- hoohacks/settings.py
+++ hoohacks/settings.py
@@ -232,6 +232,8 @@ try:
             dsn=os.environ['SENTRY_DSN'],
             integrations=[DjangoIntegration()]
         )
+        SECURE_SSL_REDIRECT = True
+        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 except ImportError:
     found = False
 
